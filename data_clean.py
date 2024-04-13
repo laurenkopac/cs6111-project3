1af862b1b1055a871c590865f3844f7f328ce594
laurenkopac <kopac.lauren@gmail.com>
2024-04-13 14:15:45 -0400

    adding main.py, data_clean.py updates

--- data_clean.py
+++ data_clean.py
@@ -11,12 +11,28 @@ import pandas as pd
 ## Assumes source file is in the same directory as data_clean.py
 source_file_name = 'restaurant_inspection_results.csv'
 
+# Target file name
+target_file_name = 'INTEGRATED-DATASET.csv'
+
 # Use pandas to read in the CSV
 df = pd.read_csv(source_file_name)
 
 # Clean columns
 df.columns = df.columns.str.replace(' ','_').str.upper()
-df.rename(columns={'DBA':'RESTAURANT_NAME'})
+df.columns = df.columns.str.replace('DESCRIPTION','DESC')
 
-# Rename Columns
-print(df.info())
+# Change DBA (doing business as) to RESTAURANT_NAME for better readability
+df.rename(columns={'DBA':'RESTAURANT_NAME'}, inplace=True)
+
+# Remove rows that do not have a score and grade
+df = df[['BORO','CUISINE_DESC','VIOLATION_CODE','CRITICAL_FLAG','GRADE']].dropna(subset=['GRADE'])
+
+# Replace NaN VIOLATION CODE with custom value: 000
+df['VIOLATION_CODE'].fillna('000',inplace=True)
+
+if df.shape[0] < 1000:
+    print("Your cleaned dataset did not yield enough rows for this project.")
+    print("Revisit your methodology and try again.")
+else:
+    # Export cleaned dataset to directory
+    df.to_csv(target_file_name, index=False)
