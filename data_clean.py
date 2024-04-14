e4ca6df20a25a05ca3e028b27a8999ed8779ec91
laurenkopac <kopac.lauren@gmail.com>
2024-04-14 11:19:37 -0400

    attempting to fix apriori algorithm. either too slow or doesn't produce results at all

--- data_clean.py
+++ data_clean.py
@@ -36,22 +36,133 @@ df = df.merge(violation_code_mapping[['VIOLATION_CODE','CATEGORY_DESC']], on='VI
 # Remove rows that do not have a score and grade
 df = df[['CAMIS','BORO','CUISINE_DESC','CATEGORY_DESC','GRADE']]
 
+# Remove rows that list category desc as "OTHER"
+df = df[df['CATEGORY_DESC'] != 'OTHER']
+
+# Remove unlisted, nondescript cuisines
+df = df[~df['CUISINE_DESC'].isin(['Not Listed/Not Applicable','Other'])]
+
 # Replace NaN VIOLATION CODE with custom value: 000
 df['CATEGORY_DESC'].fillna('NO VIOLATION FOUND',inplace=True)
 
-# Clean Descriptions to be more general
+# Clean CATEGORY_DESC to be more general
 df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('COLD HOLDING','FOOD HANDLING')
 df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('HOT HOLDING','FOOD HANDLING')
 df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('REDUCE OXYGEN PACKAGE','FOOD HANDLING')
 df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('COOLING & REFRIGERATION','FOOD HANDLING')
 df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('TEMPERATURE REGULATING','FOOD HANDLING')
+df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('REHEATING & FOOD HANDLING','FOOD HANDLING')
+df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('COOKING','FOOD HANDLING')
+df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('UNAPPROVED SOURCE','FOOD HANDLING')
 df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('FOOD WORKERS','CONTAMINATION')
 df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('ADULTERATED','CONTAMINATION')
 df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('FOOD PROTECTION','CONTAMINATION')
+df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('LABELING','CONTAMINATION')
+df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('HACCP PLAN','CONTAMINATION')
+df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('UTENSILS','WAREWASHING')
 df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('HANDWASH/TOILET','PLUMBING')
+df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('SIGNS','MAINTENANCE, CONSTRUCTION & PLACEMENT')
+df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('PERMIT/FPC','MAINTENANCE, CONSTRUCTION & PLACEMENT')
+df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('OBSTRUCTION','MAINTENANCE, CONSTRUCTION & PLACEMENT')
+df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('LIGHT, HEAT & VENTILATION','EQUIPMENT')
+df['CATEGORY_DESC'] = df['CATEGORY_DESC'].str.replace('MAINTENANCE CONSTRUCTION & PLACEMENT','MAINTENANCE, CONSTRUCTION & PLACEMENT')
+
+cuisine_mapping = {
+    'Pizza': 'Italian',
+    'Sandwiches': 'American',
+    'Chinese': 'Asian',
+    'Mexican': 'Latin American',
+    'American': 'American',
+    'Greek': 'Mediterranean',
+    'Middle Eastern': 'Middle Eastern',
+    'Italian': 'Italian',
+    'Japanese': 'Asian',
+    'Coffee/Tea': 'Beverages',
+    'Irish': 'European',
+    'Bakery Products/Desserts': 'Desserts',
+    'Thai': 'Asian',
+    'Turkish': 'Mediterranean',
+    'Chicken': 'American',
+    'Soul Food': 'American',
+    'Tex-Mex': 'Mexican',
+    'Spanish': 'European',
+    'Seafood': 'Seafood',
+    'Donuts': 'Desserts',
+    'Mediterranean': 'Mediterranean',
+    'Hotdogs': 'American',
+    'Creole': 'American',
+    'Indian': 'Asian',
+    'Caribbean': 'Latin American',
+    'Latin American': 'Latin American',
+    'French': 'European',
+    'Soups': 'American',
+    'Jewish/Kosher': 'European',
+    'Vegan': 'Vegetarian',
+    'Korean': 'Asian',
+    'Chinese/Japanese': 'Asian',
+    'Hamburgers': 'American',
+    'Salads': 'American',
+    'African': 'African',
+    'Bangladeshi': 'Asian',
+    'Fusion': 'Fusion',
+    'Asian/Asian Fusion': 'Asian',
+    'Bottled Beverages': 'Beverages',
+    'Brazilian': 'Latin American',
+    'German': 'European',
+    'Frozen Desserts': 'Desserts',
+    'Soups/Salads/Sandwiches': 'American',
+    'Juice, Smoothies, Fruit Salads': 'Beverages',
+    'Australian': 'Pacific',
+    'Sandwiches/Salads/Mixed Buffet': 'American',
+    'Continental': 'European',
+    'Filipino': 'Asian',
+    'Bagels/Pretzels': 'American',
+    'Tapas': 'European',
+    'Hawaiian': 'Pacific',
+    'Peruvian': 'Latin American',
+    'Pancakes/Waffles': 'American',
+    'Russian': 'European',
+    'New French': 'European',
+    'Polish': 'European',
+    'Eastern European': 'European',
+    'Southeast Asian': 'Asian',
+    'Barbecue': 'American',
+    'Nuts/Confectionary': 'Desserts',
+    'Steakhouse': 'American',
+    'Egyptian': 'Middle Eastern',
+    'Vegetarian': 'Vegetarian',
+    'Pakistani': 'Asian',
+    'New American': 'American',
+    'Afghan': 'Asian',
+    'Chinese/Cuban': 'Asian',
+    'Iranian': 'Middle Eastern',
+    'Cajun': 'American',
+    'English': 'European',
+    'Indonesian': 'Asian',
+    'Hotdogs/Pretzels': 'American',
+    'Ethiopian': 'African',
+    'Creole/Cajun': 'American',
+    'Californian': 'American',
+    'Lebanese': 'Middle Eastern',
+    'Portuguese': 'European',
+    'Haute Cuisine': 'European',
+    'Southwestern': 'American',
+    'Moroccan': 'Middle Eastern',
+    'Czech': 'European',
+    'Scandinavian': 'European',
+    'Chilean': 'Latin American',
+    'Armenian': 'Middle Eastern',
+    'Fruits/Vegetables': 'Vegetarian',
+    'Basque': 'European',
+    'Chimichurri': 'Latin American'
+}
+
+# Map cuisine to general categories
+df['CUISINE_DESC'] = df['CUISINE_DESC'].map(cuisine_mapping)
 
 # Drop NaN rows
 df.dropna(inplace=True)
+df.drop_duplicates(inplace=True)
 
 # Reject dataset if less than 1000 rows are left after cleaning
 if df.shape[0] < 1000:
@@ -60,10 +171,10 @@ if df.shape[0] < 1000:
     exit()
 
 # Avoid support issues with too large a dataset
-## If dataset is larger than 1000, randomly sample 1000 rows
+## If dataset is larger than 5000, randomly sample 1000 rows
 if df.shape[0] > 5000:
     # Set random_state for reproducablity, ensure each row is only sampled once
-    sampled_df = df.sample(n=5000, replace=False, random_state=123)
+    sampled_df = df.sample(n=5000, replace=False, random_state=111)
     sampled_df.to_csv(target_file_name, index=False)
 else:
     df.to_csv(target_file_name, index=False)
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ main.py
 import sys
 from itertools import combinations
 import pandas as pd
-import operator
 
 # Initialize variables for minimum support and confidence, file name
 
@@ -19,19 +18,19 @@ def cmd_line():
     """
     # If too few or too many args passed, exit and explain
     if len(sys.argv) != 4:
-        print(f"Usage: python3 main.py INTEGRATED-dfSET.csv <min_sup> <min_conf>: {len(sys.argv)}")
+        print(f"Usage: python3 main.py INTEGRATED-DATASET.csv <min_sup> <min_conf>: {len(sys.argv)}")
         exit(1)
 
     # Extract command line arguments: min_sup and min_conf
     try:
-        # Asserts the parameters passed are numbers
+        # Asserts the parameters passed are floats
         min_sup = float(sys.argv[2])
         min_conf = float(sys.argv[3])
 
         # Check to make sure parameters are greater than or equal to 0, and less than 1.
-        if (min_sup <= 0) | (min_conf <=0):
+        if (min_sup <= 0) or (min_conf <=0):
             raise ValueError("Your parameters for min_sup and min_conf cannot be less than or equal to 0.")
-        if (min_sup > 1) | (min_conf > 1):
+        if (min_sup > 1) or  (min_conf > 1):
             raise ValueError("Your parameters for min_sup and min_conf cannot be greater than 1.")
 
     except ValueError as e:
@@ -46,7 +45,7 @@ def cmd_line():
 
 def query_print_cmd(min_sup, min_conf,df):
     """
-    Display user inputted parameters and information about the dfset
+    Display user inputted parameters and information about the dataset
 
     Input: Number iteration (starting with 0 for initial search), and desired relation description
     Output: A terminal print out for the user summarizing their inputed args
@@ -65,140 +64,137 @@ def read_in_df(filename):
     Reads in df from user inputed filename
 
     INPUT: a CSV filename
-    OUTPUT: a usable dfframe
+    OUTPUT: a usable dataframe
     """
     df = pd.read_csv(filename)
 
+    df['items'] = df[['BORO',
+                      'CUISINE_DESC',
+                      'CATEGORY_DESC',
+                      'GRADE']].apply(lambda row: [str(item) for item in row], axis=1)
+    
     return df
 
-def init(df, min_sup):
-    # Extract all large itemsets with a size of 1 and calculate the support metric
-    cache = {}
-    l1 = []
-    n = df.shape[0]
-    for index, row in df.iterrows():
-        for item in row:
-            item_str = str(item)  # Convert to string
-            if len(item_str) == 0:
-                continue
-            if (item_str,) not in cache:  # Wrap item in a tuple to create a singleton itemset
-                cache[(item_str,)] = 0
-            cache[(item_str,)] += 1
-    for item in sorted(cache.keys()):
-        cur_supp = float(cache[item]) / n
-        if cur_supp >= min_sup:
-            item_sets[item] = cur_supp
-            l1.append(item)
-        else:
-            cache.pop(item)
-    return l1
+def join_sets(itemsets, k, min_sup, df):
+    """
+    Join frequent item sets of size k-1 to generate candidate item sets of size k.
+    Apply Apriori pruning by checking the subsets of the candidate itemsets.
+    """
+    new_candidate_itemsets = set()
+    hash_tree = {}  # Hash tree to efficiently store and retrieve itemsets
 
-def apriori_gen(l, k):
-    c_k = []
-    helper_set = set(l)
-    for s in l:
-        helper_set.add(s)
-    for p, q in combinations(l, 2):
-        if p[:k - 1] == q[:k - 1]:
-            new_tuple = list(p)
-            new_tuple.append(q[k - 2])
-            c_k.append(tuple(sorted(new_tuple)))
-    return c_k
+    # Construct the hash tree
+    for itemset in itemsets:
+        if len(itemset) == k - 1:
+            for subset in combinations(itemset, k - 1):
+                subset_set = frozenset(subset)
+                hash_tree[subset_set] = True
 
-def get_l(c_k, k, df, min_sup):
-    cache = {}
-    # Count the frequency
-    helper_set = set()
-    for c in c_k:
-        helper_set.add(c)
+    for itemset1 in itemsets:
+        for itemset2 in itemsets:
+            # Ensure itemset1 and itemset2 are sets of items
+            itemset1_set = set(itemset1)
+            itemset2_set = set(itemset2)
+            
+            union_set = itemset1_set.union(itemset2_set)
+            if len(union_set) == k:
+                # Generate k-size subsets
+                subsets = [frozenset(subset) for subset in combinations(union_set, k - 1)]
+                # Check if all (k-1)-size subsets are in the hash tree
+                is_valid = all(subset in hash_tree for subset in subsets)
+                if is_valid:
+                    support = sum(1 for _, transaction in df.iterrows() if union_set.issubset(transaction['items']))
+                    if support >= min_sup:
+                        new_candidate_itemsets.add(frozenset(union_set))
+                        # Add the new itemset to the hash tree
+                        hash_tree[frozenset(union_set)] = True
 
-    n = len(df)
-    for index, row in df.iterrows():
-        c_t = set()
-        for i in row:
-            c_t.add((i,))
-        for c in c_t.intersection(helper_set):
-            if c not in cache:
-                cache[c] = 1.0
-            else:
-                cache[c] += 1
-    # Calculate support and generate l_k
-    l = []
-    for c in sorted(cache.keys()):
-        cur_supp = cache[c] / n
-        if cur_supp >= min_sup:
-            item_sets[c] = cur_supp
-            l.append(c)
-    return l
 
-def apriori(df, min_sup):
-    # Generate all large itemsets
-    # l -- list type, is a table, each row is a tuple
-    l = init(df,min_sup)
+    return new_candidate_itemsets
+
+
+def generate_freq_itemsets(df, min_sup):
+    """
+    Generate candidate item sets starting from size 2.
+    """
+    # Extract individual items from the 'items' column, splitting them into words seperated by commas
+    items = [set(item.split(',')) for sublist in df['items'] for item in sublist if len(item) > 1]
+    print(items)
+    unique_items = set(frozenset(item) for item in items)
+
+    # Initialize candidate itemsets with singleton sets
+    candidate_itemsets = {item: items.count(item) for item in unique_items}
+
     k = 2
-    while len(l) != 0:
-        c_k = apriori_gen(l, k)
-        l = get_l(c_k, k, df, min_sup)
+    freq_itemsets = {}
+
+    while True:
+        new_candidate_itemsets = join_sets(candidate_itemsets.keys(), k, min_sup, df)
+        if not new_candidate_itemsets:
+            # Terminate the loop when no new candidates can be generated
+            break
+        
+        # Filter out infrequent itemsets
+        for itemset in new_candidate_itemsets:
+            support = sum(1 for _, transaction in df.iterrows() if itemset.issubset(transaction['items']))
+            if support >= min_sup:
+                freq_itemsets[itemset] = support
+
+        candidate_itemsets = {itemset: support for itemset, support in freq_itemsets.items()}
         k += 1
-    return item_sets
 
-def generate(item_sets, min_conf):
-    # Generate high-confidence rules
-    rules = {}
-    for itemset in item_sets.keys():
-        if len(itemset) == 1:
-            continue
-        for item in itemset:
-            LHS = list(itemset)
-            LHS.remove(item)
-            LHS_supp = item_sets[tuple(LHS)]
-            RHS_and_LHS_supp = item_sets[itemset]
-            if LHS_supp > 0:
-                cur_conf = RHS_and_LHS_supp / LHS_supp
-                RHS = []
-                RHS.append(item)
-                if cur_conf >= min_conf:
-                    temp = {}
-                    temp['conf'] = cur_conf
-                    temp['supp'] = item_sets[itemset]
-                    key = '[' + ', '.join(LHS) + '] => '
-                    key += '[' + str(RHS[0]) + ']'
-                    rules[key] = temp
-    return rules
+    return freq_itemsets
 
-def write_output(filename,item_sets,min_sup,min_conf):
+def generate_assoc_rules(freq_itemsets, min_conf):
+    """
+    Generate association rules from frequent item sets with a min_conf inputted by the user through cmnd
+    """
+    assoc_rules = []
+    for itemset in freq_itemsets.keys():
+        if len(itemset) > 1:
+            for i in range(1, len(itemset)):
+                for lhs in combinations(itemset, i):
+                    lhs = set(lhs)
+                    # Find RHS by removing items that are found on the LHS
+                    rhs = itemset - lhs
+                    # Calculate confidence
+                    confidence = freq_itemsets[itemset] / freq_itemsets[lhs.union(rhs)]
+                    if confidence >= min_conf:
+                        assoc_rules.append((lhs, rhs, confidence))
+    return assoc_rules
 
+def run_apriori(df, min_sup, min_conf):
+    print("Generating frequent itemsets...")
+    freq_itemsets = generate_freq_itemsets(df,min_sup)
+    print("Generating association rules...")
+    assoc_rules = generate_assoc_rules(freq_itemsets, min_conf)
+
+    return freq_itemsets, assoc_rules
+
+def write_output(filename, itemsets, assoc_rules, min_sup, min_conf):
     with open(filename, 'w') as output:
         output.write(f'====== Frequent itemsets (min_sup = {min_sup*100}%) ======\n')
-        for item_set, support in sorted(item_sets.items(), key=lambda x: x[1], reverse=True):
-            items_str = ", ".join(item_set)
-            output.write(f'       [{items_str}] -- {support*100:.2f}%\n')
-
-        # Extract rules
-        rules = generate(item_sets, min_conf)
-        sorted_rules = sorted(rules.items(), key=lambda x: x[1]['conf'], reverse=True)
+        for itemset, support in sorted(itemsets.items(), key=lambda x: x[1], reverse=True):
+            items_str = ", ".join(itemset)
+            output.write(f'       [{items_str}] -- {support:.2f}%\n')
 
         output.write('\n')
         output.write(f'====== High-confidence association rules (min_conf = {min_conf*100}%) ======\n')
-        for rule, metrics in sorted_rules:
-            lhs_items_str = ", ".join(rule.split(' => ')[0][1:-1].split(', '))
-            rhs_item_str = rule.split(' => ')[1][1:-1]
-            conf = metrics['conf']*100
-            supp = metrics['supp']*100
-            output.write(f'       [{lhs_items_str}] => [{rhs_item_str}] -- (Conf: {conf:.2f}%  Supp: {supp:.2f}%)\n')
+        for lhs, rhs, confidence in assoc_rules:
+            lhs_str = ", ".join(lhs)
+            rhs_str = ", ".join(rhs)
+            output.write(f'       [{lhs_str}] => [{rhs_str}] -- (Confidence: {confidence:.2f}%)\n')
 
 def main(min_sup,min_conf,df):
     
     query_print_cmd(min_sup,min_conf,df)
 
     print("Running A Priori algorithm...")
-    apriori(df, min_sup)
-
+    freq_itemsets, assoc_rules = run_apriori(df,min_sup,min_conf)
     print("Exporting...")
-    write_output('output.txt',item_sets,min_sup,min_conf)
+    write_output('output.txt',freq_itemsets,assoc_rules,min_sup,min_conf)
 
 if __name__ == "__main__":
-    # item_sets -- contains all large itemsets with their support
-    item_sets = {}
+    # itemsets -- contains all large itemsets with their support
     min_sup, min_conf, df = cmd_line()
     main(min_sup,min_conf,df)
